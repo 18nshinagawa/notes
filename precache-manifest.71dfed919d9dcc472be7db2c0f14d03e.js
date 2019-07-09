b9544f997760c34a4c9a15a8d3ed46022b799101
18nshinagawa <18nshinagawa.stem@gmail.com>
2019-07-08 23:41:27 -0400

    Updates

--- precache-manifest.71dfed919d9dcc472be7db2c0f14d03e.js
+++ precache-manifest.71dfed919d9dcc472be7db2c0f14d03e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/notes/static/js/runtime~main.21ba3d73.js"
   },
   {
-    "revision": "76ad6cfcde2ae5d98b89",
-    "url": "/notes/static/js/main.5c38b110.chunk.js"
+    "revision": "414bf7c9a1e62c1d13d5",
+    "url": "/notes/static/js/main.c57ac5db.chunk.js"
   },
   {
     "revision": "88e03ff9a8c281ccf157",
     "url": "/notes/static/js/2.27889932.chunk.js"
   },
   {
-    "revision": "76ad6cfcde2ae5d98b89",
+    "revision": "414bf7c9a1e62c1d13d5",
     "url": "/notes/static/css/main.009e5c8c.chunk.css"
   },
   {
-    "revision": "ca4086d1888e00b8a1c9c0c721f11c9d",
+    "revision": "058db4b0e34b079a869da704f343233a",
     "url": "/notes/index.html"
   }
 ];
